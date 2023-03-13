c7464099fdae717f171ece58d754a84856ee533f
Quinn McHugh <qpmchugh@gmail.com>
2023-03-13 10:46:50 -0400

    Embed Cal.com scheduling page

---
+++ src/components/BookingPage.js
@@ -0,0 +1,20 @@
+import Cal, { getCalApi } from "@calcom/embed-react";
+import React, { useState, useEffect } from "react";
+
+export default function BookingPage() {
+  useEffect(() => {
+    (async function () {
+      const cal = await getCalApi();
+      cal("ui", {
+        styles: { branding: { brandColor: "#50C9F0" } },
+        hideEventTypeDetails: false,
+      });
+    })();
+  }, []);
+  return (
+    <Cal
+      calLink="quinnm/meet"
+      style={{ width: "100%", height: "100%", overflow: "scroll" }}
+    />
+  );
+}
