652f0b2e66153311760a21a043b3cd48bef93fb8
Quinn McHugh <qpmchugh@gmail.com>
2024-01-28 23:08:55 -0500

    Use atom instead of RSS

--- src/components/RSSButton.js
+++ src/components/RSSButton.js
@@ -4,7 +4,7 @@ import { useLocation } from "@docusaurus/router";
 export default function RSSButton() {
   const location = useLocation();
   const blogSlug = location.pathname.split("/")[1];
-  const rssUrl = `/${blogSlug}/rss.xml`;
+  const rssUrl = `/${blogSlug}/atom.xml`;
 
   return (
     <a href={rssUrl}>
